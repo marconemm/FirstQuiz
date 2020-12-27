cd88d3f9b5784678f55fb467ea4d6b5c6bdd60b0
Marcone M. Mendonça <marconemendonca@ymail.com>
2020-12-26 23:10:14 -0300

    Refactoring the "styling options".

--- _scripts/play.js
+++ _scripts/play.js
@@ -15,7 +15,6 @@ const btn_prev = document.getElementById("btn_prev");
 const btn_next = document.getElementById("btn_next");
 
 //creating the variabes:
-let acceptingAnswers = null;
 let score = null;
 let questionsList = null;
 let avaiableQuestionsList = null;
@@ -26,12 +25,13 @@ let currentQuestion = null;
 const CORRECT_SCORE = 10;
 const MAX_QUESTIONS = 5;
 
-btn_prev.addEventListener('click', e => { //Creating the "Click" event listeners:
+//Creating the "Click" event listeners:
+btn_prev.addEventListener('click', e => { 
     currentQuestion = questionsList[currentQuestion.number - 2];
 
     if (currentQuestion !== undefined) {
         btn_next.disabled = false; // eneabling the "btn_next".
-        showingCurrentQuestion();
+        showCurrentQuestion();
         if (currentQuestion.number === 1) {
             btn_prev.disabled = true; // disabling the "btn_next".
         }
@@ -43,13 +43,35 @@ btn_next.addEventListener('click', e => {
 
     if (currentQuestion !== undefined) {
         btn_prev.disabled = false; // eneabling the "btn_prev".
-        showingCurrentQuestion();
+        showCurrentQuestion();
         if (currentQuestion.number === (questionsList.length)) {
             btn_next.disabled = true; // disabling the "btn_prev".
         }
     }
 });
 
+lbl_answersLits.forEach(answer => {
+    answer.addEventListener('click', e => { //adding the "click" event listener.
+        const selectedChoice = e.target;
+        const answeredOption = Number(selectedChoice.dataset["number"]);
+        currentQuestion.selectedAnswer = answeredOption;
+        
+        let isToPushQuestion = true;
+        for (let i = 0; i < answeredQuestionsList.length; i++) {
+            if (answeredQuestionsList[i].number === currentQuestion.number) {
+                isToPushQuestion = false;
+                currentQuestion.selectedAnswer = answeredOption;
+                break;
+            }
+        }
+
+        if (isToPushQuestion) {
+            currentQuestion.selectedAnswer = answeredOption;
+            answeredQuestionsList.push(currentQuestion);
+        }
+        showCurrentQuestion();
+    });
+});
 
 fillignLists = numerOfQuestions => { //A temp function destinated to fill the processes's lists;
 
@@ -74,34 +96,12 @@ startQuiz = () => { // It's a ES6 arrow function (The "start quiz" function).
     avaiableQuestionsList = [];
     answeredQuestionsList = [];
     fillignLists(MAX_QUESTIONS);
-    //currentQuestion = getRandomQuestion();
     currentQuestion = questionsList[0];
-    showingCurrentQuestion();
-
-    lbl_answersLits.forEach(answer => {
-
-        // Styling answers:
-        if (currentQuestion.selectedAnswer === null) {
-            answer.parentElement.classList.remove("selected-answer");
-        } else if (currentQuestion.selectedAnswer === Number(answer.dataset["number"])) {
-            answer.parentElement.classList.add("selected-answer");
-        }
-
-        answer.addEventListener('click', e => { //adding the "click" event listener.
-            if (!acceptingAnswers) return;
-
-            const selectedChoice = e.target;
-            const selectedAnswer = Number(selectedChoice.dataset["number"]);
-            acceptingAnswers = false; // changing the falg "acceptingAnswers".
-            currentQuestion.selectedAnswer = selectedAnswer;
-            answeredQuestionsList.push(currentQuestion);
-            currentQuestion = getRandomQuestion();
-            showingCurrentQuestion();
-        });
-    });
+    btn_prev.disabled = true;
+    showCurrentQuestion();
 }; // startQuiz()
 
-getRandomQuestion = () => { //The function destined to raffle some question.
+/* getRandomQuestion = () => { //The function destined to raffle some question.
 
     if (avaiableQuestionsList.length === 0) { // if true, goes to the "scores page" (./game/scores.html)
         return window.location.assign("./scores.html");
@@ -114,14 +114,23 @@ getRandomQuestion = () => { //The function destined to raffle some question.
     avaiableQuestionsList.splice(questionIndex, 1); //remove the raffled question from the avaiable list.
 
     return raffledQuestion;
-}; // getRandomQuestion( ... ) 
+}; // getRandomQuestion( ... )  */
 
-showingCurrentQuestion = () => {
+showCurrentQuestion = () => {
+    //updating HTML's texts:
     lbl_Question.innerText = currentQuestion.question;
 
     for (let i = 0; i < lbl_answersLits.length; i++) {
         lbl_answersLits[i].innerText = currentQuestion.answers[i];
     }
-}; //showingCurrentQuestion( ... )
+
+    lbl_answersLits.forEach(answer => { // Styling answers:
+        if (currentQuestion.selectedAnswer === Number(answer.dataset["number"])) {
+            answer.parentElement.classList.add("selected-answer");
+        } else {
+            answer.parentElement.classList.remove("selected-answer");
+        }
+    });
+}; //showCurrentQuestion( ... )
 
 startQuiz();
