5133b13be153a24a6f52cd5a4f4806a78807729b
Marcone M. Mendonça <marconemendonca@ymail.com>
2020-12-28 17:18:09 -0300

    Some code refactoration.

--- _scripts/play.js
+++ _scripts/play.js
@@ -9,8 +9,8 @@ class Question {
 };
 
 //Creating the constants from HTML:
-const lbl_Question = document.getElementById("question");
-const lbl_answersLits = Array.from(document.getElementsByClassName("choice-text")); //convert to an Array
+const lbl_Question = document.getElementById("lbl_question");
+const lbl_answersLits = Array.from(document.getElementsByClassName("lbl_choiceText")); //convert to an Array
 const btn_prev = document.getElementById("btn_prev");
 const btn_next = document.getElementById("btn_next");
 
@@ -24,6 +24,7 @@ let currentQuestion = null;
 //creating the processing's constants:
 const CORRECT_SCORE = 10;
 const MAX_QUESTIONS = 3;
+const WAIT_TIME = 1000;
 
 //Creating the "Click" event listeners:
 btn_prev.addEventListener('click', e => { 
@@ -31,10 +32,11 @@ btn_prev.addEventListener('click', e => {
 
     if (currentQuestion !== undefined) {
         btn_next.disabled = false; // eneabling the "btn_next".
-        showCurrentQuestion();
         if (currentQuestion.number === 1) {
             btn_prev.disabled = true; // disabling the "btn_next".
         }
+        // setTimeout(showCurrentQuestion(),WAIT_TIME);
+        showCurrentQuestion();
     }
 });
 
@@ -43,10 +45,11 @@ btn_next.addEventListener('click', e => {
 
     if (currentQuestion !== undefined) {
         btn_prev.disabled = false; // eneabling the "btn_prev".
-        showCurrentQuestion();
         if (currentQuestion.number === (questionsList.length)) {
             btn_next.disabled = true; // disabling the "btn_prev".
         }
+        // setTimeout(showCurrentQuestion()   ,WAIT_TIME);
+        showCurrentQuestion();
     }
 });
 
@@ -73,9 +76,9 @@ lbl_answersLits.forEach(answer => {
     });
 });
 
-fillignLists = numerOfQuestions => { //A temp function destinated to fill the processes's lists;
+fillignLists = numberOfQuestions => { //A temp function destinated to fill the processes's lists;
 
-    for (let i = 1; i <= numerOfQuestions; i++) {
+    for (let i = 1; i <= numberOfQuestions; i++) {
         const correctAnswer = Math.floor(Math.random() * 4);
         const answersList = [];
 
