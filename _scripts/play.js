de2c68943253da6552c51f6fc26c83e6dad35c0b
Marcone M. Mendonça <marconemendonca@ymail.com>
2021-01-15 22:43:18 -0300

    A huge logic change.

--- _scripts/play.js
+++ _scripts/play.js
@@ -19,7 +19,10 @@ const lbl_hud = document.getElementById("lbl_hud");
 const progressBarContent = document.getElementById("loadingBarContent");
 const lbl_progressBar = document.getElementById("lbl_progressBar");
 const lbl_question = document.getElementById("lbl_question");
-const lbl_answersLits = Array.from(document.getElementsByClassName("lbl_choiceText")); //convert to an Array
+// const lbl_choiceOptions = Array.from(document.getElementsByClassName("lbl_choiceOption")); //convert to an Array
+// const rb_choiceOptions = Array.from(document.getElementsByClassName("radioStyles"));//convert to an Array
+// const lbl_answers = Array.from(document.getElementsByClassName("bl_choiceText")); //convert to an Array
+const choicesOptions = Array.from(document.getElementsByClassName("choice-container")); //convert to an Array
 const btn_prev = document.getElementById("btn_prev");
 const btn_next = document.getElementById("btn_next");
 const btn_finish = document.getElementById("btn_finish");
@@ -27,8 +30,8 @@ const btn_finish = document.getElementById("btn_finish");
 //creating the variabes:
 let score;
 let questionsList;
-let avaiableQuestionsList;
-let answeredQuestionsList;
+//let avaiableQuestionsList;
+let answeredQuestions;
 let currentQuestion;
 let raffledCategory;
 const difficulty = JSON.parse(sessionStorage.getItem("difficulty"));
@@ -40,79 +43,127 @@ const player = new Player(JSON.parse(sessionStorage.getItem("playerName")));
 //creating the processing's constants:
 const CORRECT_SCORE = 10;
 const MAX_QUESTIONS = 10;
-const WAIT_TIME = 1000;
 const CATEGORIES_URL = "https://opentdb.com/api_category.php";
 
 //Creating the "Click" event listeners:
 btn_prev.addEventListener('click', e => {
-    currentQuestion = questionsList[currentQuestion.number - 2];
-
+    currentQuestion = questionsList[currentQuestion.questionNumber - 2];
+    
     if (currentQuestion !== undefined) {
         btn_next.disabled = false; // eneabling the "btn_next".
-        if (currentQuestion.number === 1) {
+        if (currentQuestion.questionNumber === 1) {
             btn_prev.disabled = true; // disabling the "btn_next".
         }
         // setTimeout(renderScreen(),WAIT_TIME);
-        renderScreen();
+        renderScreen(true);
     }
 });
 
 btn_next.addEventListener('click', e => {
-    currentQuestion = questionsList[currentQuestion.number];
+    currentQuestion = questionsList[currentQuestion.questionNumber];
 
     if (currentQuestion !== undefined) {
         btn_prev.disabled = false; // eneabling the "btn_prev".
-        if (currentQuestion.number === (questionsList.length)) {
+        if (currentQuestion.questionNumber === (questionsList.length)) {
             btn_next.disabled = true; // disabling the "btn_prev".
         }
-        // setTimeout(renderScreen()   ,WAIT_TIME);
-        renderScreen();
+        renderScreen(true);
     }
 });
 
 btn_finish.addEventListener('click', e => {
-    if (answeredQuestionsList.length === MAX_QUESTIONS) { // if true, goes to the "scores page" (./game/scores.html)
+    if (answeredQuestions === MAX_QUESTIONS) { // if true, goes to the "scores page" (./game/scores.html)
         return window.location.assign("./scores.html");
     }
 });
 
-lbl_answersLits.forEach(answer => {
-    answer.parentElement.addEventListener('click', e => { //adding the "click" event listener on the answer container (the parent).
-        const selectedChoice = e.target;
-        const answeredOption = Number(selectedChoice.dataset["number"]);
+for (let i = 0; i < choicesOptions.length; i++) {
+    
+    choicesOptions[i].addEventListener('click', e => {
+        const clickedOption = Number(choicesOptions[i].dataset["option"]);
 
-
-        let isToPushQuestion = true;
-
-        for (let i = 0; i < answeredQuestionsList.length; i++) {
-
-            if (answeredQuestionsList[i].number === currentQuestion.number) { //Updates the answeres questions list:
-                isToPushQuestion = false;
-
-                if (currentQuestion.selectedAnswer === null || answeredOption !== currentQuestion.selectedAnswer) {
-                    //console.log(`Updating the answer ${answeredOption} to the question number ${currentQuestion.number}.`);
-                    currentQuestion.selectedAnswer = answeredOption;
-                    answeredQuestionsList[i].selectedAnswer = answeredOption;
-                }
-                else {
-                    //console.log(`Nulling the answer from the question number ${currentQuestion.number}.`);
-                    currentQuestion.selectedAnswer = null;
-                    answeredQuestionsList.splice(i,1); //removes the nulled answer from answeredQuestionsList.
-                }
-
-                break;
+        currentQuestion.selectedAnswer = (currentQuestion.selectedAnswer !== clickedOption) ? clickedOption : undefined;
+        
+        let count = 0;
+        questionsList.forEach(question => {
+            if (question.selectedAnswer !== undefined) {
+                count++;
             }
-        }
+        });
 
-        if (isToPushQuestion) {
-            // console.log(`Pushing the question number ${currentQuestion.number}. into answered questions list.`);
-            currentQuestion.selectedAnswer = answeredOption; //updates the asweredOption.
-            answeredQuestionsList.push(currentQuestion);
-        }
+        answeredQuestions = count;
+
+        renderScreen(false);
+
+    }); 
+}
+/*
+choicesOptions.forEach(option => {
+    option.parentElement.addEventListener('click', e => { //adding the "click" event listener on the option container (the parent).
+        const selectedChoice = e.target;
+        console.log(selectedChoice.dataset["option"]);
+        currentQuestion.selectedAnswer = Number(selectedChoice.dataset["option"]);
+        
+        // choicesOptions.forEach(rb => {
+        //     if (rb.checked) {
+        //         console.log(rb);
+        //         //console.log(rb.label);
+        //         console.log(lbl_choiceOptions);
+        //         rb.checked = false;
+        //     }
+        // });
+        
+
+        //console.log("\n\n");
         renderScreen();
+        
+        // let isToPushQuestion = true;
+
+        // for (let i = 0; i < answeredQuestionsList.length; i++) {
+
+        //     if (answeredQuestionsList[i].number === currentQuestion.number) { //Updates the answeres questions list:
+        //         isToPushQuestion = false;
+
+        //         if (currentQuestion.selectedAnswer === null || answeredOption !== currentQuestion.selectedAnswer) {
+        //             //console.log(`Updating the answer ${answeredOption} to the question number ${currentQuestion.number}.`);
+        //             currentQuestion.selectedAnswer = answeredOption;
+        //             answeredQuestionsList[i].selectedAnswer = answeredOption;
+        //         }
+        //         else {
+        //             //console.log(`Nulling the answer from the question number ${currentQuestion.number}.`);
+        //             currentQuestion.selectedAnswer = null;
+        //             answeredQuestionsList.splice(i,1); //removes the nulled answer from answeredQuestionsList.
+        //         }
+
+        //         break;
+        //     }
+        // }
+
+        // if (isToPushQuestion) {
+        //     // console.log(`Pushing the question number ${currentQuestion.number}. into answered questions list.`);
+        //     currentQuestion.selectedAnswer = answeredOption; //updates the asweredOption.
+        //     answeredQuestionsList.push(currentQuestion);
+        // }
+        //renderScreen();
     });
 });
+*/
 
+// Creating the functions:
+const raflleCategory = async () => {
+    
+    const trivia_categories = await fetch(CATEGORIES_URL) //fetch a response promise form "CATEGORIES_URL"
+    .then(response => response.json()) // parse the response promise to a JSON promise
+    .then(jsonPromise => { // raffle a category from the "jsonPromise":
+    
+    const raffledIndex = Math.round(Math.random() * jsonPromise.trivia_categories.length);
+            raffledCategory = jsonPromise.trivia_categories[raffledIndex].id;
+            
+        });
+        
+        
+}; //raflleCategory
+    
 const mapQuestionsList = async fetchResult => {
 
     questionsList = await fetchResult.map(question => {
@@ -127,40 +178,33 @@ const mapQuestionsList = async fetchResult => {
     });
 
     for (let i = 0; i < questionsList.length; i++) {
-       
+        
         questionsList[i].questionNumber = i + 1;
         
     }
     
     currentQuestion = questionsList[0];
-    renderScreen();
+    renderScreen(true);
 
 }; //mapQuestionsList
-
-const raflleCategory = async () => {
-    
-    const trivia_categories = await fetch(CATEGORIES_URL) //fetch a response promise form "CATEGORIES_URL"
-        .then(response => response.json()) // parse the response promise to a JSON promise
-        .then(jsonPromise => { // raffle a category from the "jsonPromise":
-           
-            const raffledIndex = Math.round(Math.random() * jsonPromise.trivia_categories.length);
-            raffledCategory = jsonPromise.trivia_categories[raffledIndex].id;
-    
-        });
-    
-
-}; //raflleCategory
 
 const fetchQuestions = async (amount, difficulty) => {
 
     await raflleCategory(); //raflle a category
     
     //fetch the "triviaQuestionsList":
-    const TRIVIA_ADRESS = isRandomCategory ?
-    `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`:
-    `https://opentdb.com/api.php?amount=${amount}&category=${raffledCategory}&difficulty=${difficulty.toLowerCase()}&type=multiple`;
+    const TRIVIA_API = `https://opentdb.com/api.php?amount=${amount}&type=multiple`;
+    let fetchUrl = "";
     
-    const triviaQuestionsList = await fetch(TRIVIA_ADRESS)
+    if (isRandomCategory && difficulty === "all") {
+        fetchUrl = TRIVIA_API;
+    } else if (isRandomCategory) {
+        fetchUrl = `${TRIVIA_API}&difficulty=${difficulty}`;
+    } else {
+        fetchUrl = `${TRIVIA_API}&category=${raffledCategory}`;
+    }
+    
+    const triviaQuestionsList = await fetch(fetchUrl)
         .then(response => response.json())
         .then(jsonPromise => {
             
@@ -170,37 +214,34 @@ const fetchQuestions = async (amount, difficulty) => {
 
 }; //fetchQuestions
 
-fetchQuestions(MAX_QUESTIONS, difficulty);
+/*const fillignLists = numberOfQuestions => { //A temp function destinated to fill the processes's lists;
 
-// const fillignLists = numberOfQuestions => { //A temp function destinated to fill the processes's lists;
+    for (let i = 1; i <= numberOfQuestions; i++) {
+        const correctAnswer = Math.floor(Math.random() * 4);
+        const answersList = [];
 
-//     for (let i = 1; i <= numberOfQuestions; i++) {
-//         const correctAnswer = Math.floor(Math.random() * 4);
-//         const answersList = [];
+        for (let j = 1; j <= 4; j++) {
+            const lbl_answerTxt = `Resposta ${j} da questão ${i}`;
+            answersList.push(lbl_answerTxt);
+        }
 
-//         for (let j = 1; j <= 4; j++) {
-//             const lbl_answerTxt = `Resposta ${j} da questão ${i}`;
-//             answersList.push(lbl_answerTxt);
-//         }
+        const lbl_questionTxt = `Qual a resposta para esta questão?`;
 
-//         const lbl_questionTxt = `Qual a resposta para esta questão?`;
+        avaiableQuestionsList.push(new Question(i, lbl_questionTxt, answersList, correctAnswer));
+    };
 
-//         avaiableQuestionsList.push(new Question(i, lbl_questionTxt, answersList, correctAnswer));
-//     };
+    questionsList = [...avaiableQuestionsList]; //make a copy of the questions list.
 
-//     questionsList = [...avaiableQuestionsList]; //make a copy of the questions list.
-
-// }; // fillignLists(numerOfQuestions)
+}; // fillignLists(numerOfQuestions)
+*/
 
 const resetQuiz = () => { // It's a ES6 arrow function (The "start quiz" function).
     score = undefined;
     questionsList = undefined;
     avaiableQuestionsList = undefined;
-    answeredQuestionsList = [];
-    //fillignLists(MAX_QUESTIONS);
-    //currentQuestion = questionsList[0];
+    answeredQuestions = undefined;
     btn_prev.disabled = true;
-    //renderScreen();
+    fetchQuestions(MAX_QUESTIONS, difficulty);
 }; // resetQuiz()
 
 /* getRandomQuestion = () => { //The function destined to raffle some question.
@@ -218,30 +259,37 @@ const resetQuiz = () => { // It's a ES6 arrow function (The "start quiz" functio
     return raffledQuestion;
 }; // getRandomQuestion( ... )  */
 
-const renderScreen = async () => {
-    //updating HTML's texts:
-    lbl_question.innerText = currentQuestion.questionTxt;
+const renderScreen = async isToRenderTheTexts => {
     
-    for (let i = 0; i < lbl_answersLits.length; i++) {
-       lbl_answersLits[i].innerText = currentQuestion.answersList[i];
+    if (isToRenderTheTexts) { //rendering HTML's texts:
+        lbl_hud.innerText = `Questão: ${currentQuestion.questionNumber} de ${questionsList.length}`;
+        
+        lbl_question.innerText = currentQuestion.questionTxt;
+        
+        for (let i = 0; i < choicesOptions.length; i++) {
+            
+            choicesOptions[i].innerHTML = `<span class="lbl_choiceOption">${choicesOptions[i].dataset["char"]}</span>
+            <input class="radioStyles" type="radio" name="rbAnswers" id="op_1">
+            <span class="lbl_choiceText">${currentQuestion.answersList[i]}</span>`
+        }
+
     }
     
-    lbl_answersLits.forEach(answer => { // Styling answers:
-        if (currentQuestion.selectedAnswer === Number(answer.dataset["number"])) {
-            answer.parentElement.classList.add("selected-answer");
+    choicesOptions.forEach(option => { //Styling answers:
+
+        if (currentQuestion.selectedAnswer === Number(option.dataset["option"])) {
+            option.classList.add("selected-answer");
         } else {
-            answer.parentElement.classList.remove("selected-answer");
+            option.classList.remove("selected-answer");
         }
     });
 
-    lbl_hud.innerText = `Questão: ${currentQuestion.questionNumber} de ${questionsList.length}`;
-
     //Updating the preogress bar:
-    const newWidth = Math.round((answeredQuestionsList.length / MAX_QUESTIONS) * 100);
+    const newWidth = Math.round((answeredQuestions / MAX_QUESTIONS) * 100);
     lbl_progressBar.innerText = `${newWidth}%`;
     progressBarContent.style.width = `${newWidth}%`;
     
-    if (answeredQuestionsList.length === MAX_QUESTIONS) {
+    if (answeredQuestions === MAX_QUESTIONS) {
         //console.log("All the questions has aswers.\nEneabling the btn_finish");
         btn_finish.hidden = false;
     } else if (!btn_finish.hidden) {
@@ -250,4 +298,5 @@ const renderScreen = async () => {
     }
 }; //renderScreen( ... )
 
+// Starting the quiz:
 resetQuiz();
