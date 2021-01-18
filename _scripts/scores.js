9398bd76793a93c7296a2a6ab0317a032fdf39d5
Marcone M. Mendonça <marconemendonca@ymail.com>
2021-01-18 01:49:24 -0300

    Fixing "scores.js".

--- _scripts/scores.js
+++ _scripts/scores.js
@@ -3,34 +3,20 @@ const resultBox = document.getElementById("resultBox");
 const historyTable = document.getElementById("scoresTable");
 
 //Geting the sessionStorage values:
-const correctAnswers = Number(sessionStorage.getItem("correctAswers"));
 const playerName = sessionStorage.getItem("playerName");
 const playersHistoryList = JSON.parse(localStorage.getItem("playersHistoryList"));
+const questionsList = JSON.parse(sessionStorage.getItem("questionsList"));
+
+//Common variables:
+let correctAnswers = 0;
 
 // Creating the functions:
-const getScore = () => {
-    const questionsList = JSON.parse(sessionStorage.getItem("questionsList"));
+
+const saveTheResult = score => {
     
-    const score = questionsList.reduce((count, question ) => {
-        if (question.selectedAnswer === question.correctAnswer) {
-            if (question.difficulty === "easy")
-                count += count * 33
-            else if (question.difficulty === "medium")
-                count += count * 66
-            else
-                count += count * 99
-        }
-        return count;
-    }, 1);
-
-    return (score - 1);
-}; // getScore()
-
-const saveTheResult = () => {
-
     const dataToSave = {
         playerName : playerName,
-        score: getScore()
+        score: score
     };
     
     if (playersHistoryList !== null) {
@@ -39,10 +25,18 @@ const saveTheResult = () => {
         localStorage.setItem("playersHistoryList", JSON.stringify(playersHistoryList));
     }
     setHistoryTable();
+    resultBox.classList.add("hidden");
 }; // saveTheResult()
 
-const setHistoryTable = () => {
+const getTableHeader = () => // setup the "scoresTable" header:
+    `<tr>
+    <th>Nome</th>
+    <th>Pontuação</th>
+    </tr>`
+// getTableHeader()
 
+const setHistoryTable = () => {
+    
     playersHistoryList.sort((scoreA, scoreB) => { // sort the "playersHistoryList" by "high scores first":
         return (scoreA.score < scoreB.score) ? 1 : (scoreA.score > scoreB.score) ? -1 : 0;
     });
@@ -50,61 +44,100 @@ const setHistoryTable = () => {
     // Set the "historyTable" HTML:
     historyTable.innerHTML = playersHistoryList.reduce((newHTML, history) => 
         newHTML += `<tr> <td>${history.playerName}</td>
-        <td>${history.score}</td></tr>`
-        , historyTable.innerHTML);
+        <td>${history.score}&nbsp;<i>pts</i></td></tr>`
+        , getTableHeader());
 }; // setHistoryTable();
 
-const setResultBox = () => {
-
-    if (correctAnswers >= 9) {
-
+const setResultBox = score => {
+    
+    if (correctAnswers >= 9) 
         resultBox.innerHTML = `<h1>Parabéns, ${playerName}!</h1>
         <label for="result">Você acertou</label>
         <h2 id="result">${correctAnswers} questões!</h2>
-        <h3>E sua pontuação foi: ${getScore()} pontos.</h3>
+        <h3>E sua pontuação foi: ${score} pontos.</h3>
         <button class="btn" id="btn_save">Salvar</button>`
-
-    } else if (correctAnswers < 9 && correctAnswers > 7) {
-        
+    
+    else if (correctAnswers < 9 && correctAnswers > 7)  
         resultBox.innerHTML = `<h1>Muito bem, ${playerName}!</h1>   
         <label for="result">Você acertou</label>
         <h2 id="result">${correctAnswers} questões!</h2>
-        <h3>E sua pontuação foi: ${getScore()} pontos.</h3>
+        <h3>E sua pontuação foi: ${score} pontos.</h3>
         <button class="btn" id="btn_save">Salvar</button>`
-
-    } else if (correctAnswers > 1) {
-        
+    
+    else if (correctAnswers > 1)     
         resultBox.innerHTML = `<h1>${playerName},</h1>   
         <label for="result">Você acertou apenas</label>
         <h2 id="result">${correctAnswers} questões.</h2>
-        <h3>E sua pontuação foi: ${getScore()} pontos.</h3>
+        <h3>E sua pontuação foi: ${score} pontos.</h3>
         <button class="btn" id="btn_save">Salvar</button>`
-        
-    } else if (correctAnswers === 1) {
+    
+    else if (correctAnswers === 1)
         resultBox.innerHTML = `<h1>${playerName},</h1>   
         <label for="result">Você acertou apenas</label>
         <h2 id="result">${correctAnswers} questão.</h2>
-        <h3>E sua pontuação foi: ${getScore()} pontos.</h3>
+        <h3>E sua pontuação foi: ${score} pontos.</h3>
         <button class="btn" id="btn_save">Salvar</button>`
-
-    } else {
-
+        
+    else
         resultBox.innerHTML = `<h1>${playerName},</h1>   
         <h2>Infelizmente Você errou todas as questões.</h2>`
-    }
-
+        
+        
 }; // setResultBox()
+    
+const getScore = () => {
+
+    console.log(questionsList);
+    
+    const score = questionsList.reduce((count, question ) => {
+        
+        console.log(question.selectedAnswer === question.correctAnswer);
+        if (question.selectedAnswer === question.correctAnswer) {
+            console.log(correctAnswers);
+            correctAnswers++;
+            console.log(correctAnswers);
+
+            if (question.difficulty === "easy")
+                count += 33;
+            else if (question.difficulty === "medium")
+                count += 66;
+            else
+                count += 99;
+
+        }
+
+        return count;
+
+    }, 1);
+    
+    return (score - 1);
+}; // getScore()
 
 const renderScreen = () => {
-    setResultBox();
+    const score = getScore();
     setHistoryTable();
 
-    const btn_save = document.getElementById("btn_save");
-
-    btn_save.addEventListener("click", e => {
-        saveTheResult();
-    });
-
+    if (playerName !== null) {
+        const btn_home = document.getElementById("btn_home");
+        
+        btn_home.innerText = "Reiniciar";
+        setResultBox(score);
+        
+        const btn_save = document.getElementById("btn_save");
+        if (btn_save !== null)
+        btn_save.addEventListener("click", e => {
+            historyTable.innerHTML = getTableHeader();
+            saveTheResult(score);
+        });
+        
+        resultBox.classList.remove("hidden");
+    } else
+    resultBox.classList.add("hidden");
+    
 } // renderScreen()
+
+const goToHome = () => {
+    window.location.assign("/");
+}
 
 renderScreen();
